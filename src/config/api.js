aad9db0aa55b5ea3b02d3c5db62cc4fb1f5de195
Amurag <anuragsinghisgood@gmail.com>
2026-04-01 10:48:51 +0530

    made changes to the backend url call

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-// const fallbackApiBaseUrl = "https://storybook-backend-payment.onrender.com";
-const fallbackApiBaseUrl = "http://localhost:3000";
+const fallbackApiBaseUrl = "https://storybook-backend-payment.onrender.com";
+// const fallbackApiBaseUrl = "http://localhost:3000";
 
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL?.trim() || fallbackApiBaseUrl;
